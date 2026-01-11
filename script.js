d94a65053d7a60b2abf9efbeaaefd2411fe48ac2
Asim Aljma'an <109857678+OGLoco07@users.noreply.github.com>
2026-01-11 17:04:32 +0300

    Update script.js

--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function normalize(text) {
 /***********************
   LOAD CSV
 ************************/
-fetch('شركات.csv')
+fetch('data.csv')
     .then(res => res.text())
     .then(data => {
         const rows = data.split('\n').filter(r => r.trim() !== '');
